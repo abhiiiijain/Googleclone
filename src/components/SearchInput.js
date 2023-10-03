5eae521f90401e3521593424bfb60c1a55062669
Abhinandan Jain <abhinandanbansal123@gmail.com>
2023-10-03 20:55:27 +0530

    homepage layout created

---
+++ src/components/SearchInput.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyledInputWrapper } from "./Searchinput.Styled";
+
+const SearchInput = () => {
+  return (
+    <form>
+      <div>
+        <StyledInputWrapper>
+          
+        </StyledInputWrapper>
+      </div>
+    </form>
+  );
+};
+
+export default SearchInput;
---
+++ src/components/Searchinput.Styled.js
@@ -0,0 +1,6 @@
+import { styled } from "@mui/system";
+import SearchIcon from "@mui/icons-material/Search";
+
+const StyledInputWrapper = styled("div")``;
+
+export { StyledInputWrapper };
